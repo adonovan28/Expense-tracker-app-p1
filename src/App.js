112854eae482ada225156cbfd2ac42a11455604f
Instructor Austin <instructoraustin@Software-Dev-iMac-Pro.local>
2023-04-26 11:57:11 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./styles/App.scss";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+// Components
+import MainContainer from "./components/Containers/MainContainer";
+import PageContainer from "./components/Containers/PageContainer";
+import Navbar from "./components/Navbar/Navbar";
+import MobileNavbar from "./components/Navbar/MobileNavbar";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import { AuthProvider } from "./context/AuthProvider";
+
+// Pages
+import Auth from "./pages/Auth";
+import Home from "./pages/Home";
+
+//REACT QUERY
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "./constants/config";
+import Categories from "./pages/Categories";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <PageContainer optionClass={"pageContainer"}>
+            <Navbar />
+            <div className="mobileMenu">
+              <MobileNavbar />
+            </div>
+            <Routes>
+              {/* AUTH PAGE */}
+              <Route path="/auth" element={<Auth />} />
+              {/* Categories */}
+              <Route path="/categories" element={<Categories />} />
+              {/* PROTECTED ROUTES */}
+              <Route element={<ProtectedRoutes />}>
+                {/* HOME */}
+                <Route path="/" element={<Home />} />
+                {/* Categories */}
+                <Route path="/categories" element={<Categories />} />
+                {/* 404 */}
+                <Route
+                  path="/*"
+                  element={
+                    <MainContainer>
+                      <span style={{ fontSize: "1.2rem" }}>
+                        404 Page Not Found
+                      </span>
+                    </MainContainer>
+                  }
+                />
+              </Route>
+            </Routes>
+          </PageContainer>
+        </AuthProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </div>
   );
 }
